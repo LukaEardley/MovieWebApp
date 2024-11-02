d04b766fb0ea9cefcdf46b9e293c9e3ecefd243a
LukaEardley <lukaeardley01@gmail.com>
2024-11-02 11:54:50 +0200

    Deleted the entirety of the UI as too many collisions and vulnerabilities, Starting from clean slate

--- MovieClient/movie-gold-v1/src/api/axiosConfig.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-    baseURL: 'https://9c96-103-106-239-104.ap.ngrok.io/', //Ngrok URL to open locl server to public
-    headers: { "ngrok-skip-browser-warning": "true" } //CORS blocker
-
-});
